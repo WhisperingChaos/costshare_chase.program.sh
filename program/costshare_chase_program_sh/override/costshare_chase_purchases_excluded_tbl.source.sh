b9fc8d4acb481d397e8b910d0cc28c3e9e2afa48
Rich Moyse <rich@moyse.us>
2022-05-04 23:05:04 -0400

    improve coments and insert historical test entry

--- program/costshare_chase_program_sh/override/costshare_chase_purchases_excluded_tbl.source.sh
+++ program/costshare_chase_program_sh/override/costshare_chase_purchases_excluded_tbl.source.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 costshare_chase_purchases_excluded_tbl(){
-# nothing to exclude so far.  Note first line is removed from stream
+# Note first column heading line is removed from this stream.
+# Note the entry ",01/01/1900" is used to test the program.
+# Since the rows below can include a limiting time component (transactionDateRegex)
+# time bounded entries can remain in the table "forever".  Retaining
+# these entries permits rerunning the program with historical data that should,
+# if there have been no changes to the historical data sets, produce exactly 
+# the same outcome.  However, peserving past time bounded entries will slow
+# the program's performance when processing data sets whose purchases lie 
+# outside bounded periods.  
 cat <<'purchases_excluded_tbl'
-<vendorNameRegex>,<dateRegex>,<amtRegex>
+<vendorNameRegex>,<transactionDateRegex>,<amtRegex>
+,01/01/1900
 purchases_excluded_tbl
 }
