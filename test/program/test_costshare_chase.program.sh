7dcf050b987d5c53471f386af10ad65faf227b9d
Rich Moyse <rich@moyse.us>
2023-02-11 20:46:38 -0500

    change test expected compares to accomidate new CSV header.  Simplify testing framework when adding more statement tests

--- test/program/test_costshare_chase.program.sh
+++ test/program/test_costshare_chase.program.sh
@@ -13,24 +13,28 @@ test_costshare_chase_program__compose(){
 
 
 test_costshare_chase_program__OKI_POKIE(){
-  assert_true '
-    echo "03/27/2021,03/28/2021,OKI POKE,Food & Drink,Sale,-16.97," \
-    | ./costshare_chase.program.sh  2>&1  \
-    |  assert_output_true test_costshare_chase__main_OKI_POKIE_expected'
+  assert_output_true test_costshare_chase__main_OKI_POKIE_expected \
+    --- test_costshare_chase_program__OKI_POKIE_generate
+}
+test_costshare_chase_program__OKI_POKIE_generate(){
+  echo "03/27/2021,03/28/2021,OKI POKE,Food & Drink,Sale,-16.97," \
+   | ./costshare_chase.program.sh ./component_override/OKI_POKIE
 }
 test_costshare_chase__main_OKI_POKIE_expected(){
 cat<<'OKI_POKIE'
-TransactionDate,VendorName,Charge,OffSpngPctShare,OffSpngShareAmt,ParentShareAmt,Category
-03/27/2021,OKI POKE,-16.97,29,-4.92,-12.05,Food & Drink
+TransactionDate,VendorName,Charge,PartyXpct,SharePartyXRound,SharePartyY,Category,Type
+03/27/2021,OKI POKE,-16.97,29,-4.92,-12.05,Food & Drink,Sale
 OKI_POKIE
 }
 
 
 test_costshare_chase_program__OKI_POKIE_BJS(){
-  assert_true '
-    test_costshare_chase__main_OKI_POKIE_BJS_in \
-    | ./costshare_chase.program.sh  2>&1  \
-    | assert_output_true test_costshare_chase__main_OKI_POKIE_BJS_expected'
+  assert_output_true test_costshare_chase__main_OKI_POKIE_BJS_expected \
+    --- test_costshare_chase_program__OKI_POKIE_BJS_generate
+}
+test_costshare_chase_program__OKI_POKIE_BJS_generate(){
+  test_costshare_chase__main_OKI_POKIE_BJS_in \
+  | ./costshare_chase.program.sh ./component_override/OKI_POKIE
 }
 test_costshare_chase__main_OKI_POKIE_BJS_in(){
 cat<<'OKI_POKIE_BJS'
@@ -40,18 +44,20 @@ OKI_POKIE_BJS
 }
 test_costshare_chase__main_OKI_POKIE_BJS_expected(){
 cat<<'OKI_POKIE_BJS'
-TransactionDate,VendorName,Charge,OffSpngPctShare,OffSpngShareAmt,ParentShareAmt,Category
-03/27/2021,OKI POKE,-16.97,29,-4.92,-12.05,Food & Drink
-03/27/2021,BJS FUEL,-30.00,50,-15.00,-15.00,Gas
+TransactionDate,VendorName,Charge,PartyXpct,SharePartyXRound,SharePartyY,Category,Type
+03/27/2021,OKI POKE,-16.97,29,-4.92,-12.05,Food & Drink,Sale
+03/27/2021,BJS FUEL,-30.00,50,-15.00,-15.00,Gas,Sale
 OKI_POKIE_BJS
 }
 
 
 test_costshare_chase_program__OKI_POKIE_fail_BJS(){
-  assert_true '
-    test_costshare_chase__main_OKI_POKIE_fail_BJS_in \
-    | ./costshare_chase.program.sh 2>&1  \
-    | assert_output_true test_costshare_chase__main_OKI_POKIE_fail_BJS_expected'
+  assert_output_true test_costshare_chase__main_OKI_POKIE_fail_BJS_expected \
+    --- test_costshare_chase_program__OKI_POKIE_fail_BJS_generate
+}
+test_costshare_chase_program__OKI_POKIE_fail_BJS_generate(){
+  test_costshare_chase__main_OKI_POKIE_fail_BJS_in \
+  | ./costshare_chase.program.sh ./component_override/OKI_POKIE
 }
 test_costshare_chase__main_OKI_POKIE_fail_BJS_in(){
 cat<<'OKI_POKIE_BJS'
@@ -61,9 +67,9 @@ OKI_POKIE_BJS
 }
 test_costshare_chase__main_OKI_POKIE_fail_BJS_expected(){
 cat<<OKI_POKIE_BJS
-TransactionDate,VendorName,Charge,OffSpngPctShare,OffSpngShareAmt,ParentShareAmt,Category
+TransactionDate,VendorName,Charge,PartyXpct,SharePartyXRound,SharePartyY,Category,Type
 03/27/2021,03/28/2021,BJS Fuel,Gas,Sale,-30.00,
-03/27/2021,OKI POKE,-16.97,29,-4.92,-12.05,Food & Drink
+03/27/2021,OKI POKE,-16.97,29,-4.92,-12.05,Food & Drink,Sale
 ${assert_REGEX_COMPARE}msgType='Fatal' msg='at least one error or unaccounted for purchase was detected. view output of STDERR' .+$
 ${assert_REGEX_COMPARE}\+  Caller: file=.+$
 ${assert_REGEX_COMPARE}\+  Caller: file=.+$
@@ -71,30 +77,27 @@ ${assert_REGEX_COMPARE}\+  Caller: file=.+$
 OKI_POKIE_BJS
 }
 
-test_costshare_chase_program_ChaseMarch(){
-
-  assert_output_true test_costshare_chase_program_ChaseMarch_expected \
-                 --- test_costshare_chase_program_ChaseMarch_generate
+test_costshare_chase_program_Chase(){
+  assert_output_true test_costshare_chase_program_Chase_expected march \
+    ---              test_costshare_chase_program_Chase_generate march
 }
-test_costshare_chase_program_ChaseMarch_generate(){
-    cat ./data/ChaseMarch.csv            \
-    | ./costshare_chase.program.sh  2>&1 \
-    | sort
+test_costshare_chase_program_Chase_generate(){
+  cat ./data/$1/Statement.csv \
+  |   ./costshare_chase.program.sh ./component_override/$1 \
+  |   sort
 }
-test_costshare_chase_program_ChaseMarch_expected(){
-  cat ./data/ChaseMarchExpected.csv | sort
+test_costshare_chase_program_Chase_expected(){
+  cat ./data/$1/SharedExpensesExpected.csv \
+  |   sort
 }
 
 test_costshare_chase_program__main(){
 
   test_costshare_chase_program__compose "$0"
 
-  test_costshare_chase_program_ChaseMarch
-return
   test_costshare_chase_program__OKI_POKIE
-  test_costshare_chase_program__OKI_POKIE_BJS
   test_costshare_chase_program__OKI_POKIE_fail_BJS
-  test_costshare_chase_program_ChaseMarch
+  test_costshare_chase_program_Chase march
 
   assert_return_code_set
 }
